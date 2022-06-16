28f021e90d2839b24525bad0ffd749f63192bb2e
Shuhei Kuroda <shu.0915@gmail.com>
2022-06-17 02:49:26 +0900

    Sheet部分完成

--- CustomTransition/BottomSheet/BottomSheet.swift
+++ CustomTransition/BottomSheet/BottomSheet.swift
@@ -12,33 +12,38 @@ struct BottomSheet: View {
   @State var offsetY: CGFloat = 0
 
   var body: some View {
-    VStack {
-      BottomSheetContentView()
-    }
-    .frame(maxWidth: .infinity, maxHeight: .infinity)
-    .background(.white)
-    .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
-    .offset(y: translation.height + offsetY)
-    .gesture(
-      DragGesture()
-        .onChanged { value in
-          translation = value.translation
-        }
-        .onEnded { value in
-          withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.7)) {
-            let snap = translation.height + offsetY
-
-            if snap > 100 {
-              offsetY = 400
-            } else {
-              offsetY = 0
-            }
-
-            translation = .zero
+    GeometryReader { proxy in
+      VStack {
+        BottomSheetContentView()
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(.white)
+      .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+      .offset(y: translation.height + offsetY)
+      .gesture(
+        DragGesture()
+          .onChanged { value in
+            translation = value.translation
           }
-        }
-    )
+          .onEnded { value in
+            withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.7)) {
+              let snap = translation.height + offsetY
+              let quarter = proxy.size.height / 4
+
+              if snap > quarter && snap < quarter * 3 {
+                offsetY = quarter * 2
+              } else if snap > quarter * 3 {
+                offsetY = quarter * 3 + 100
+              } else {
+                offsetY = 0
+              }
+
+              translation = .zero
+            }
+          }
+      )
     .ignoresSafeArea(edges: .bottom)
+    }
   }
 }
 
